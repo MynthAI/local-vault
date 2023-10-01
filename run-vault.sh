1d00b5080238358b60fcc70a9bb13d5d654ff8a5
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2023-10-01 10:32:45 +0200

    Output instruction to define token

--- run-vault.sh
+++ run-vault.sh
@@ -28,3 +28,10 @@ docker run \
     --restart unless-stopped quay.io/mynth/local-vault \
         > /dev/null
 wait_for_vault
+
+token="$(docker exec vault token)"
+if [ "$token" != "$VAULT_CLI_TOKEN" ]; then
+    echo "VAULT_CLI_TOKEN needs to be defined."
+    echo "Add the following to your shell profile:"
+    echo "export VAULT_CLI_TOKEN=\"$token\""
+fi
